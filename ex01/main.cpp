0cc6d43b0ed96c996e701fe443d318eff79d60ec
Amine Meskine <ameskine@c2r4p3.1337.ma>
2026-01-03 09:31:22 +0100

    classes object namespace

---
+++ ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameskine <ameskine@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 15:22:55 by ameskine          #+#    #+#             */
+/*   Updated: 2026/01/03 09:17:37 by ameskine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    PhoneBook book;
+    std::string command;
+    
+    while (1)
+    {
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+        if (!std::getline(std::cin, command)); //getline(input_stream, string_variable);
+            break;
+        if (command == "ADD")
+            book.addContact();
+        else if (command == "SEARCH")
+            book.searchContact();
+        else if (command == "EXIT")
+            break;
+    }
+    return (0);
+}
