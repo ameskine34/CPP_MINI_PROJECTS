73b281a74bf0c180807518292af7f243ae247711
ameskine34 <amimimeskine@gmail.com>
2026-05-19 12:13:26 +0100

    cpp06

--- C++_06/ex00/Convert.cpp
+++ C++_06/ex00/Convert.cpp
@@ -117,33 +117,48 @@ void isInt(const char *s)
     std::cout << "double : " << std::fixed << std::setprecision(2) << static_cast<double>(num) << std::endl;
 }
 
+int ft_strlen(const char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] == '.')
+            break;
+        i++;
+    }
+    return (i);
+}
+
 bool isFloatLiteral(const char *s)
 {
     int i = 0;
     char *end;
-    float num;
 
     if (s[0] == '-' || s[0] == '+')
         i++;
     if (!s[i])
         return (false);
-    while (s[i])
-    {
-        if (!std::isdigit(s[i]))
-        {
-            num = std::strtol(s, &end, 10);
-            if (strlen(end) > 2 || end[0] != '.'|| end[1] != 'f' )
-            {
-                std::cout << "inappropriate form" << std::endl; 
-                return (false);
-            }
-        }
-        i++;
-    }
-    if (num < FLT_MIN || num > FLT_MAX)
+    if (ft_strlen(s) > 18)
     {
         std::cout << "out of range" << std::endl;
         return (false);
+    }
+    std::strtol(&s[i], &end, 10);
+    if (end[0] != '.' || end[1] == 'f')
+    {
+        std::cout << "inappropriate form" << std::endl;
+        return (false);
+    }
+    int j = 0;
+    while (end[++j])
+    {
+        if (!std::isdigit(end[j]))
+        {
+            if (end[j] == 'f' && end[j + 1] == '\0')
+                return (true);
+            std::cout << "inappropriate form" << std::endl;
+            return (false);
+        }
     }
     return (true);
 }
@@ -167,10 +182,44 @@ void isFloat(const char *s)
     }
     if (value >= FLT_MIN || value <= FLT_MAX)
     {
-        std::cout << "float : " << std::fixed << std::setprecision(2) << static_cast<float>(value)<< 'f' << std::endl;    
+        std::cout << "float : " << std::fixed << std::setprecision(2) << value << 'f' << std::endl;    
     }
     std::cout << "double : " << std::fixed << std::setprecision(2) << static_cast<double>(value) << std::endl;
 }
+
+bool isDoubleLiteral(const char *s)
+{
+    int i = 0;
+    char *end;
+
+    if (s[0] == '-' || s[0] == '+')
+        i++;
+    if (!s[i])
+        return (false);
+    if (ft_strlen(s) > 18)
+    {
+        std::cout << "out of range" << std::endl;
+        return (false);
+    }
+    std::strtol(&s[i], &end, 10);
+    if (end[0] != '.')
+    {
+        std::cout << "inappropriate form" << std::endl;
+        return (false);
+    }
+    int j = 0;
+    while (end[++j])
+    {
+        if (!std::isdigit(end[j]))
+        {
+            std::cout << "inappropriate form" << std::endl;
+            return (false);
+        }
+    }
+    return (true);
+}
+
+
 
 void ScalarConverter::convert(const std::string s)
 {
@@ -182,8 +231,8 @@ void ScalarConverter::convert(const std::string s)
         isInt(n);
     else if (isFloatLiteral(n))
         isFloat(n);
-    // else if (isDoubleLiteral(n))
-    //     isDouble(n);
+    else if (isDoubleLiteral(n))
+        isDouble(n);
     // else
     //     isSpecial(n);
 }
