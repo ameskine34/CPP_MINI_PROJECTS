c0fa9286148785ff25d8b431189266c9294a520b
ameskine34 <amimimeskine@gmail.com>
2026-06-16 10:11:51 +0100

    cpp07

--- C++_07/test.cpp
+++ C++_07/test.cpp
@@ -7,10 +7,18 @@ T my_max(T a, T b)
     return (a > b) ? a : b;
 }
 
-int main()
-{
-    std::cout << my_max(45, 20) << std::endl;
-}
+// int main()
+// {
+//     std::cout << my_max(45, 20) << std::endl;
+// }
 
 //template is a blueprint not a code, template <typename T> make
 //my_max a generic function that works with any data type
+
+int main()
+{
+    // int x = 5;
+    // double y = 10.5;
+
+    std::cout << my_max<double>(4, 5.8) << std::endl;
+}
