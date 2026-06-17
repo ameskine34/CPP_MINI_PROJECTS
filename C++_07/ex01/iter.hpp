cbff3f45e26f25d497c20969027647ad4429e114
ameskine34 <amimimeskine@gmail.com>
2026-06-17 16:52:11 +0100

    cpp07

--- C++_07/ex01/iter.hpp
+++ C++_07/ex01/iter.hpp
@@ -4,14 +4,14 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *add, size_t lenght, void (*func)(T&))
+void iter(T *add, size_t lenght, void (*func)(T))
 {
     for (size_t i = 0; i < lenght; i++)
         func(add[i]);
 }
 
 template <typename T>
-void function(T& string)
+void function(T string)
 {
     std::cout << "this is the function output :" << string << std::endl;
 }
