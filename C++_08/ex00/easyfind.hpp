9dc4655160510ae114d9816fba5c61af368991a6
ameskine34 <amimimeskine@gmail.com>
2026-07-05 20:43:44 +0100

    cpp08

--- C++_08/ex00/easyfind.hpp
+++ C++_08/ex00/easyfind.hpp
@@ -1,12 +1,31 @@
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+// #ifndef EASYFIND_HPP
+// #define EASYFIND_HPP
 
-#include <iostream>
+// #include <iostream>
+
+// template <typename T>
+// int easyfind(T *num, int to_find)
+// {
+//     for(int i = 0; i < )
+// }
+
+// #endif
+
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <stdexcept>
 
 template <typename T>
-int easyfind(T *num, int to_find)
+typename T::iterator easyfind(T &container, int value)
 {
-    for(int i = 0; i < )
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+	if (it == container.end())
+		throw "easyfind: value not found";
+	return (it);
 }
 
 #endif
---
+++ C++_08/ex00/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+int main()
+{
+	std::vector<int> vec;
+	for (int i = 0; i < 10; i++)
+		vec.push_back(i * 2);
+	try
+	{
+		std::vector<int>::iterator it = easyfind(vec, 100);
+		std::cout << "Before: " << *it << std::endl;
+		*it = 999;
+		std::cout << "After: " << *it << std::endl;
+	}
+	catch (const char* msg)
+	{
+		std::cout << msg << std::endl;
+	}
+	return 0;
+}
