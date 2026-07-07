c7b9855eb6f4e4e3b3a8c340d658efdf10bef64c
ameskine34 <amimimeskine@gmail.com>
2026-07-07 15:47:54 +0100

    cpp

--- C++_08/ex02/MutantStack.hpp
+++ C++_08/ex02/MutantStack.hpp
@@ -4,18 +4,40 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <stack>
 
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    MutantStack();
-    ~MutantStack();
-    MutantStack(const MutantStack& other);
-    MutantStack& operator=(const MutantStack& other);
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-    typename std::stack<T>::container_type::iterator begin();
-    typename std::stack<T>::container_type::iterator end();
+    MutantStack(){
+    };
+    MutantStack(const MutantStack& other)
+    {
+        *this = other;
+    };
+    MutantStack& operator=(const MutantStack& other){
+        if (this != &other)
+            std::stack<T>::operator=(other);
+        return *this;
+    };
+    iterator begin(){
+        return std::stack<T>::c.begin();
+    };
+    iterator end(){
+        return std::stack<T>::c.end();
+    };
+    const_iterator begin() const{
+        return std::stack<T>::c.begin();
+    };
+    const_iterator end() const{
+        return std::stack<T>::c.end();
+    };
+    ~MutantStack(){
+    };
 };
 
 #endif
