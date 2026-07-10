12c1d4276232bea89c298aad98f6c8e120d90032
ameskine34 <amimimeskine@gmail.com>
2026-07-10 11:27:16 +0100

    cpp

---
+++ C++_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,16 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class BitcoinExchange
+{
+    private:
+    public:
+        BitcoinExchange();
+        BitcoinExchange& operator=(const BitcoinExchange& other);
+        ~BitcoinExchange();
+};
+#endif
