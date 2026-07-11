480c5f07e4d5610823740332110523762eeb4944
ameskine34 <amimimeskine@gmail.com>
2026-07-11 14:02:09 +0100

    cpp

--- C++_09/ex00/BitcoinExchange.hpp
+++ C++_09/ex00/BitcoinExchange.hpp
@@ -1,16 +1,38 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <string>
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+//for the stringstream that treat the line as stream that we can manipultate
 
-class BitcoinExchange
+typedef struct Data
 {
-    private:
-    public:
-        BitcoinExchange();
-        BitcoinExchange& operator=(const BitcoinExchange& other);
-        ~BitcoinExchange();
-};
+    std::string date;
+    std::string price;
+}Data;
+
+
+// class BitcoinExchange
+// {
+//     private:
+//     public:
+//         BitcoinExchange();
+//         BitcoinExchange& operator=(const BitcoinExchange& other);
+//         ~BitcoinExchange();
+// };
+
+bool isValidDate(const std::string& dateStr);
+
 #endif
+
+//date format : R"(^(\d{4})-(\d{2})-(\d{2})$)"
+//A raw string literal tells the compiler to ignore escape 
+//sequences (like \n, \t, or \") ---- *^* the start of the string
+//(\d{4}) capturing the group 
+//  \d digit from [0-9]
+
+//std::smatch matches; string matches
--- C++_09/ex00/main.cpp
+++ C++_09/ex00/main.cpp
@@ -1,10 +1,89 @@
 #include "BitcoinExchange.hpp"
 
+bool isValidDate(const std::string& s)
+{
+    if (s.length() != 10)
+        return false;
+
+    if (s[4] != '-' || s[7] != '-')
+        return false;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 4 || i == 7)
+            continue;
+        if (!isdigit(s[i]))
+            return false;
+    }
+    int year = std::atoi(s.substr(0,4).c_str());
+    int month = std::atoi(s.substr(5,2).c_str());
+    int day = std::atoi(s.substr(8,2).c_str());
+    if (month < 1 || month > 12)
+        return (false);
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (month == 2)
+    {
+        bool isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        if (isLeap) {
+            daysInMonth[1] = 29;
+        }
+    }
+    if (day < 1 || day > daysInMonth[month - 1])
+        return (false);
+    return (true);
+}
+
+bool isValid(const std::string& s)
+{
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '.' && isdigit(s[i + 1]))
+            continue;
+        if (!isdigit(s[i]))
+            return false;
+    }
+    float price = std::atoi(s.c_str());
+    if (price < 0)
+        return (false);
+    return (true);
+}
+
 int main(int ac, char **av)
 {
-    if (ac != 2)
+    std::vector<Data> data_list;
+    std::ifstream file(av[1]);
+    if (ac != 2 || !file.is_open())
     {
         std::cout << "Error: could not open file." << std::endl;
-        return (0);
+        return (1);
     }
+    std::string line;
+    if (std::getline(file, line)){
+    }//line skipped once line is read the cursur move to the next line
+    while (std::getline(file, line))
+    {
+        Data dd;
+        std::stringstream ss(line);
+        std::string extract;
+        if (std::getline(ss, extract, ',')) dd.date = extract;
+        if (std::getline(ss, extract, '\n')) dd.price = extract;
+        if (!isValidDate(dd.date) || !isValid(dd.price))
+        {
+            std::cout << "Error: invalid date format." << std::endl;
+            return (1);
+        }
+        data_list.push_back(dd);
+        std::cout << dd.date << std::endl;
+        std::cout << dd.price << std::endl;
+    }        
+    file.close();
+    return (0);
 }
+
+//file.csv (Comma-Separated Values)
+//std::stringstream why using it cuz getline 1st parameter should be
+//input stream
+//In the Gregorian calendar, a year is a leap year if:
+//It is divisible by 4, except
+//Years divisible by 100 are not leap years, except
+//Years divisible by 400 are leap years.
